e8aec54d2ffa68e3721064fbc1d0f422bec318f3
Alberto Beltran <albertobeltran@protonmail.com>
2023-10-22 13:58:55 -0600

    set up server to handle login POST req

--- server.tsx
+++ server.tsx
@@ -1,18 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+//* Middleware
+
+// middleware to allow request from different origin
+app.use(cors());
+
+// middleware to read body, parse it and place results in req.body
+app.use(express.json());             // for application/json
+app.use(express.urlencoded());       // for application/x-www-form-urlencoded
+
+//* Routes
+
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
 
 app.post('/login', (req, res) => {
-    res.send('Done')
+    console.log(req.body);
+    res.send('Done');
 });
+
+//* Setup
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,52 +4,63 @@ import { useState } from 'react';
 const Login = () => {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
-    
+
     const handleSubmit = (e: any) => {
         e.preventDefault();
-        
-        fetch('http://127.0.0.1:5500', {
+
+        console.log(`Employee ${id} with password ${password}`);
+
+        fetch('http://localhost:3000/login', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ id_emp: id, password: password })
+            body: JSON.stringify({
+                id_emp: id,
+                password: password
+            })
         })
-        .then(
-        // Fulfilled
-            () => {
-            console.log('Done')
-        // Rejected
-        }, () => {
-            console.log('Not done')
-        })
+            .then(
+                // Fulfilled
+                () => {
+                    console.log('Done')
+                    // Rejected
+                }, () => {
+                    console.log('Not done')
+                })
 
-
-        // alert(`Employee ${id} with password ${password}`);
+        // Reset form after submit
+        setId('');
+        setPassword('');
     }
 
     const handleUserId = (e: any) => setId(e.target.value)
-
     const handleUserPassword = (e: any) => setPassword(e.target.value)
 
     return (
         <>
             <form onSubmit={handleSubmit} action="/login">
+                {/* Id input */}
                 <label htmlFor="id_emp">Expediente: </label>
                 <input
                     type="text"
                     name="id_emp"
                     id="id_emp"
                     onChange={handleUserId}
+                    // Adopt state value to reset after submit
+                    value={id}
                 />
-
+                {/* Password input */}
                 <label htmlFor="password">Clave: </label>
                 <input
                     type="password"
                     name="password"
                     id="password"
                     onChange={handleUserPassword}
+                    // Adopt state value to reset after submit
+                    value={password}
                 />
+                {/* Submit */}
                 <input type="submit" value="Acceder" />
             </form>
         </>
